0afe06fc65fb6f1ac208bc3f47dc0da87d925df5
Jason Lin <jasonlin43212@gmail.com>
2019-03-13 18:36:04 -0400

    added basic function definitions

---
+++ splay.py
@@ -0,0 +1,38 @@
+class Node:
+
+    def __init__(self,value):
+        self.value = value
+        self.less = None
+        self.greater = None
+
+class SplayTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self):
+        return
+
+    def remove(self, node):
+        return
+
+    def find(self, node):
+        return
+
+    def rotate_left(self, node):
+        return
+
+    def rotate_right(self, node):
+        return
+
+    def zig(self, node):
+        return
+
+    def zig_zag(self, node):
+        return
+
+    def zig_zig(self,node):
+        return
+
+    def print_tree(self):
+        return
